79bb9c43b8d2c9e55ac56dec3cdf0c88e6e9157c
Hengki Sihombing <hengki.sihombing@gmail.com>
2016-03-03 16:52:58 +0700

    add growth as subscription type

--- src/rules.js
+++ src/rules.js
@@ -2,7 +2,8 @@ const PUBLISH_JOBS_FREE = 1
 const PUBLISH_JOBS_STARTER = 3
 const PUBLISH_JOBS_STANDARD = 5
 const PUBLISH_JOBS_PROFESSIONAL = 10
-const PUBLISH_JOBS_ENTERPRISE = 999
+const PUBLISH_JOBS_GROWTH = 20
+const PUBLISH_JOBS_ENTERPRISE = 40
 
 // const COMPANY_PLAN_ARRAY        = ['beta', 'trial', 'free', 'starter', 'standard', 'profesional', 'enterprise']
 const COMPANY_PLAN_BETA = 'beta'
@@ -10,6 +11,7 @@ const COMPANY_PLAN_TRIAL = 'trial'
 const COMPANY_PLAN_FREE = 'free'
 const COMPANY_PLAN_STARTER = 'starter'
 const COMPANY_PLAN_STANDARD = 'standard'
+const COMPANY_PLAN_GROWTH = 'growth'
 const COMPANY_PLAN_PROFESSIONAL = 'profesional'
 const COMPANY_PLAN_ENTERPRISE = 'enterprise'
 
@@ -36,6 +38,10 @@ module.exports = {
 
       case COMPANY_PLAN_STANDARD:
         allowToCreate = currentPublishCount < PUBLISH_JOBS_STANDARD
+        break
+
+      case COMPANY_PLAN_GROWTH:
+        allowToCreate = currentPublishCount < PUBLISH_JOBS_GROWTH
         break
 
       case COMPANY_PLAN_PROFESSIONAL:
